d287b4fe3655ca822639c9ce547a031a697831ff
omar <omarrayad74@gmail.com>
2024-05-01 17:43:51 +0300

    add more projects done

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -13,7 +13,15 @@ const FirstSection = () =>{
             case 4: window.open('https://omarayadd.github.io/javaScriptProject1/', '_blank');
                     break;        
             case 5: window.open('https://github.com/omarayadd/Bachelor-Project', '_blank');
-                    break;                   
+                    break;
+            case 6: window.open('https://omarayadd.github.io/laptopStore/', '_blank');
+                    break;  
+            case 7: window.open('https://omarayadd.github.io/geminiAI/', '_blank');
+                    break;
+            case 8: window.open('https://omarayadd.github.io/todo-list/', '_blank');
+                    break;
+            case 9: window.open('https://tasktrial.onrender.com/', '_blank');
+                    break;                       
             default: window.open('https://www.facebook.com', '_blank');
         }
         
@@ -53,6 +61,32 @@ const FirstSection = () =>{
                         <div className="box-body">
                             <p>Skills: Python, openGym</p>
                             <a href="#" onClick={()=>openWeb(5)}>Show more..</a>
+                        </div>
+                    </div>
+                    <div className="box box6">
+                        <div className="box-body">
+                            <p>Skills: javaScript, React</p>
+                            <a href="#" onClick={()=>openWeb(6)}>Show more..</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="box box7">
+                        <div className="box-body">
+                            <p>Skills: javaScript, React</p>
+                            <a href="#" onClick={()=>openWeb(7)}>Show more..</a>
+                        </div>
+                    </div>
+                    <div className="box box8">
+                        <div className="box-body">
+                            <p>Skills: javaScript, React, Node.js, Express, MongoDB</p>
+                            <a href="#" onClick={()=>openWeb(8)}>Show more..</a>
+                        </div>
+                    </div>
+                    <div className="box box9">
+                        <div className="box-body">
+                            <p>Skills: Node.js, Express, MongoDB</p>
+                            <a href="#" onClick={()=>openWeb(9)}>Show more..</a>
                         </div>
                     </div>
                 </div>
